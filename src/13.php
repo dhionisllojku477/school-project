3bd85b2c100cdc62f03e4b4ff71591c6b1ca9f2b
dhionisllojku477 <dhionisllojku477@gmail.com>
2025-03-18 09:48:08 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,10 @@
+<?php
+// Define an array with some numbers
+$numbers = array(1, 2, 3, 4, 5);
+
+// Use the "array_sum" function to calculate the sum of the numbers
+$sum = array_sum($numbers);
+
+// Print the result
+echo "The sum of the numbers is $sum";
+?>
