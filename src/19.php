4cd933e863039dc921b84d45a8b49c4bc9077f16
dhionisllojku477 <dhionisllojku477@gmail.com>
2025-03-24 09:53:38 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,15 @@
+<?php
+// Example of a simple PHP code to demonstrate basic logic.
+// You can modify this example to fit your specific needs.
+
+// Example 1: Simple if statement
+if (true) {
+    echo "This is true.";
+}
+
+// Example 2: Simple while loop
+$i = 0;
+while ($i < 5) {
+    echo "Iteration $i of 5." . PHP_EOL;
+    $i++;
+}
