3a80672efc97d99a24476f4de46eff593cc7a6d1
dhionisllojku477 <dhionisllojku477@gmail.com>
2025-03-29 06:04:48 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,9 @@
+<?php
+// This is a PHP script to demonstrate basic functionality.
+// It can be used as an example in any programming projects or educational applications.
+
+// Example of using a simple loop to print numbers from 1 to 10
+for ($i = 1; $i <= 10; $i++) {
+    echo "Iteration: $i";
+}
+?>
