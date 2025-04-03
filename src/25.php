0c2702a02507068e1ba840df0cfaaefe98440450
dhionisllojku477 <dhionisllojku477@gmail.com>
2025-04-03 13:34:30 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,12 @@
+<?php
+// Define your variables here
+$student_id = 12345;
+$password = "secret";
+
+// Create a session for authentication
+session_start();
+
+if ($_POST) {
+    // Process login or registration logic here
+}
+?>
