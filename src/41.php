542fab5ae4edff43e6667250c3e3ee9f43573c06
dhionisllojku477 <dhionisllojku477@gmail.com>
2025-05-23 06:24:38 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,12 @@
+<?php
+// Simple PHP code to demonstrate basic operations
+$first_number = 5;
+$second_number = 3;
+
+// Perform addition
+$result = $first_number + $second_number;
+echo "The sum of $first_number and $second_number is: $result";
+
+// Perform subtraction
+$difference = $first_number - $second_number;
+echo "The difference between $first_number and $second_number is: $difference";
